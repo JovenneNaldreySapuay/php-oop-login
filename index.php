793a746afcfb7c0e25407f42104396cd81d1df47
jovenne <jovenneregs@gmail.com>
2019-05-29 13:08:02 +0800

    updated index.php

--- index.php
+++ index.php
@@ -1,38 +1,12 @@
 <?php  
-// https://github.com/aantal85/PHP-OOP-Login-Register-System
-// stop here: https://www.youtube.com/watch?v=rWon2iC-cQ0&list=PLfdtiltiRHWF5Rhuk7k4UAU1_yLAZzhWc&index=11
-
 require_once 'core/init.php';
-
-// $user = DB::getInstance()->query("SELECT * FROM users");
-// $user = DB::getInstance()->get('users', array('username', '=', 'jovenne'));
-
-// if(!$user->count()) {
-// 	echo 'No user';
-// } else {
-// 	echo $user->first()->username;
-// }
-
-// $user = DB::getInstance()->insert('users', array(
-// 	'username' => 'Gary',
-// 	'password' => 'pass123',
-// 	'salt'     => 'salt',
-// 	'name'     => 'Gary Vay'
-// ));
-
-// $user = DB::getInstance()->update('users', 5, array(
-// 	'password' => 'strong123',
-// 	'name' => 'Gary Vaynerchuk'
-// ));
 
 if(Session::exists('home')) {
 	echo Session::flash('home');
 }
 
-// echo Session::get(Config::get('session/session_name'));
-
 $user = new User();
-// echo $user->data()->username;
+
 if($user->isLoggedIn()) { ?>
 <p>Hello <a href="profile.php?user=<?php echo escape($user->data()->username); ?>"><?php echo escape($user->data()->username); ?></a>!</p>
 <ul>
